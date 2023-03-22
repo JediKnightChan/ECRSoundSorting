3a08f93911f08db4682efbfe21249c46d447e827
JediKnightChan <jediknightchannel@yandex.ru>
2023-03-22 20:58:37 +0300

    Remove csrf in signup

--- backend/myauth/urls.py
+++ backend/myauth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
+from django.views.decorators.csrf import csrf_exempt
 from .views import RecaptchaTokenObtainPairView, CreateUserView
 
 urlpatterns = [
-    path('signup/', CreateUserView.as_view(), name='signup'),
+    path('signup/', csrf_exempt(CreateUserView.as_view()), name='signup'),
     path('token/', RecaptchaTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
